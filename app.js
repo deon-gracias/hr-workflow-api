2f0ea50b8270314323afa0016954991ffcd9e4d6
deon-gracias <deongracias1@gmail.com>
2022-07-28 18:13:57 +0530

    Renamed from employee to users

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const employeeRouter = require('./routes/employee')
+const usersRouter = require('./routes/users')
 
 const app = express();
 
@@ -15,6 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/employee', employeeRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-const { } = require("../controllers/employee")
+const { } = require("../controllers/users")
 
 
 router.get("/", async (req, res, next) =>
-    res.status(200).send({ message: "Employee PATH" })
+    res.status(200).send({ message: "Users PATH" })
 )
 
 module.exports = router
