019a55c93c3108060027b3c349e4c45ccbbc75f6
deon-gracias <deongracias1@gmail.com>
2022-07-28 18:57:13 +0530

    Removed view engine

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Routes
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users')
 
+// Express App
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
 
 module.exports = app;
