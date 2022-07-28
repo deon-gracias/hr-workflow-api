3121c17bc5544e58d4ba90797449af20cafe8e20
deon-gracias <deongracias1@gmail.com>
2022-07-28 19:01:16 +0530

    Refactored

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,11 +1,14 @@
-const request = require('supertest')
-const app = require('../app')
+const request = require("supertest");
+const app = require("../app");
 
 describe("Users", () => {
-    // Check if server is working
-    it("should receive 200", async () => {
-        const res = await request(app).get("/users").expect("Content-Type", /json/).expect(200)
+  // Check if server is working
+  it("should receive 200", async () => {
+    const res = await request(app)
+      .get("/users")
+      .expect("Content-Type", /json/)
+      .expect(200);
 
-        return res
-    })
-})
+    return res;
+  });
+});
