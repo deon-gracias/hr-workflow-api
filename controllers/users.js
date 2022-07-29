fbeb8680780341c1245a1e026f798dca585c1d18
deon-gracias <deongracias1@gmail.com>
2022-07-29 18:57:27 +0530

    Removed console log

--- controllers/users.js
+++ controllers/users.js
@@ -22,7 +22,6 @@ async function getUser(req, res, next) {
   if (!user) return res.status(404).send({ message: "User not found" });
 
   delete user.password;
-  console.log(user);
 
   return res.status(200).send(user);
 }
