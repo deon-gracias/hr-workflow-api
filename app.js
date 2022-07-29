c076024f1fb539d9b5992ec76b3d6d45d02368da
deon-gracias <deongracias1@gmail.com>
2022-07-29 18:53:03 +0530

    Reformmated

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 // Routes
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users')
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
 // Express App
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: err.message });
+  res.status(500).send({ message: err.message });
 });
 
 module.exports = app;
